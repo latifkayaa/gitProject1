539ccdcab26ddb5c74950c8041ecbcb23b8649e7
Latif Kaya <latif.kayaa@outlook.com>
2022-04-08 22:53:58 +0600

    work 2 is done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("Hi");
         System.out.println("Hello World");
+        System.out.println("Hello Cydeo");
+
     }
 }
