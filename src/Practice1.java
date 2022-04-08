fac64c87e8df08419e1fdb063bc3a93d6b9541de
Latif Kaya <latif.kayaa@outlook.com>
2022-04-09 02:08:48 +0600

    Second class is in work-3

--- src/Practice1.java
+++ src/Practice1.java
@@ -3,5 +3,6 @@ public class Practice1 {
     public static void main(String[] args) {
         System.out.println("Hi!");
         System.out.println("Hello");
+        System.out.println("Hey there");
     }
 }
